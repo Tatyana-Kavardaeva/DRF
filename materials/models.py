51e57ee1dd58e62856cae92bb770ae297231994e
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-10-06 17:04:11 +0300

    updated materials\models.py added owner`s field for Course and Lesson

--- materials/models.py
+++ materials/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from config import settings
 
 
 class Course(models.Model):
@@ -7,6 +8,8 @@ class Course(models.Model):
     image = models.ImageField(
         upload_to="media/course", verbose_name="Изображение", blank=True, null=True
     )
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True,
+                              verbose_name='Владелец')
 
     class Meta:
         verbose_name = "Курс"
@@ -25,6 +28,8 @@ class Lesson(models.Model):
     video = models.URLField(
         max_length=200, verbose_name="Ссылка на видео", blank=True, null=True
     )
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True,
+                              verbose_name='Владелец')
 
     class Meta:
         verbose_name = "Урок"
