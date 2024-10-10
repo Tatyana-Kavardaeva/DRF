e356621d7cb40c15ae4744ff02d19bb721e904e5
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-10-10 23:01:04 +0300

    added class SubscriptionSerializeriew in users\serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 
-from users.models import User, Payment
+from users.models import User, Payment, Subscription
 
 
 class UserSerializer(ModelSerializer):
@@ -14,3 +14,8 @@ class PaymentSerializer(ModelSerializer):
         model = Payment
         fields = '__all__'
 
+
+class SubscriptionSerializer(ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
