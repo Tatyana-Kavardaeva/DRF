0a2da4cfdac4ed60179304230f1e776ac8223c75
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-10-10 23:03:27 +0300

    updated urls.py

--- materials/urls.py
+++ materials/urls.py
@@ -9,16 +9,16 @@ from materials.views import (CourseViewSet, LessonCreateAPIView,
 app_name = MaterialsConfig.name
 
 router = SimpleRouter()
-router.register("", CourseViewSet)
+router.register('', CourseViewSet)
 
 urlpatterns = [
-    path("lessons/", LessonListAPIView.as_view(), name="lessons-list"),
-    path("lessons/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lessons-retrieve"),
-    path("lessons/create/", LessonCreateAPIView.as_view(), name="lesson-create"),
+    path('lessons/', LessonListAPIView.as_view(), name="lessons-list"),
+    path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name="lessons-retrieve"),
+    path('lessons/create/', LessonCreateAPIView.as_view(), name="lesson-create"),
     path(
-        "lessons/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lesson-delete"
+        'lessons/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name="lesson-delete"
     ),
     path(
-        "lessons/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson-update"
+        'lessons/<int:pk>/update/', LessonUpdateAPIView.as_view(), name="lesson-update"
     ),
 ] + router.urls
--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
 from users.apps import UsersConfig
 from users.views import UserViewSet, PaymentListAPIView, PaymentRetrieveAPIView, PaymentCreateAPIView, \
-    PaymentDestroyAPIView, PaymentUpdateAPIView
+    PaymentDestroyAPIView, PaymentUpdateAPIView, SubscriptionCreateAPIView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -13,17 +13,20 @@ app_name = UsersConfig.name
 
 router = SimpleRouter()
 router.register('', UserViewSet)
+# router.register('subscription', SubscriptionViewSet)
 
 urlpatterns = [
-    path("payments/", PaymentListAPIView.as_view(), name="payment_list"),
-    path("payments/<int:pk>/", PaymentRetrieveAPIView.as_view(), name="payment_retrieve"),
-    path("payments/create/", PaymentCreateAPIView.as_view(), name="payment_create"),
+    path("payments/", PaymentListAPIView.as_view(), name="payment-list"),
+    path("payments/<int:pk>/", PaymentRetrieveAPIView.as_view(), name="payment-retrieve"),
+    path("payments/create/", PaymentCreateAPIView.as_view(), name="payment-create"),
     path(
-        "payments/<int:pk>/delete/", PaymentDestroyAPIView.as_view(), name="payment_delete"
+        "payments/<int:pk>/delete/", PaymentDestroyAPIView.as_view(), name="payment-delete"
     ),
     path(
-        "payments/<int:pk>/update/", PaymentUpdateAPIView.as_view(), name="payment_update"
+        "payments/<int:pk>/update/", PaymentUpdateAPIView.as_view(), name="payment-update"
     ),
     path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token-refresh'),
+
+    path('subscriptions/', SubscriptionCreateAPIView.as_view(), name='subscription-create'),
 ] + router.urls
