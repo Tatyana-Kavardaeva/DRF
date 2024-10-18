7b7d92d96c01ca3fa38861ffe72e066ed14d1709
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-10-18 18:36:43 +0300

    updated settings.py, admin.py, .env.sample

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,8 @@ INSTALLED_APPS = [
     'materials',
     'drf_yasg',
     'corsheaders',
+
+    'django_celery_beat',
 ]
 
 MIDDLEWARE = [
@@ -89,9 +91,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru-RU"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
@@ -122,14 +124,61 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',  # Замените на адрес вашего фронтенд-сервера
+    os.getenv('LOCALHOST'),  # Замените на адрес вашего фронтенд-сервера
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:8000',  #  Замените на адрес вашего фронтенд-сервера
-    'http://localhost:8000',  # и добавьте адрес бэкенд-сервера
+    os.getenv('LOCALHOST'),  # Замените на адрес вашего фронтенд-сервера
+    os.getenv('LOCALHOST'),  # и добавьте адрес бэкенд-сервера
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
 
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
+
+# Celery Configuration Options
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CELERY_BROKER_URL = os.getenv('LOCATION')
+CELERY_RESULT_BACKEND = os.getenv('LOCATION')
+
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+CELERY_BEAT_SCHEDULE = {
+    "deactivation_user": {
+        "task": "users.tasks.deactivation_user",
+        "schedule": timedelta(days=1),
+    },
+}
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', False) == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', False) == 'True'
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'config': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+}
--- materials/admin.py
+++ materials/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from materials.models import Lesson
+from materials.models import Lesson, Course
 
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
